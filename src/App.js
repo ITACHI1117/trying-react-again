5c84cadb16553cfcee7b2e9bbca806eaea203d64
ITACHI1117 <ajogujoseph0317@gmail.com>
2023-02-26 19:35:27 +0100

    started my Travel Journal app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Airbnb from "./pages/Airbnb";
 import Card from "./pages/Card";
+import TJournal from "./pages/TJournal";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Card />} />
         <Route path="/airbnb" element={<Airbnb />} />
+        <Route path="/tjournal" element={<TJournal />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/TJournal.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/TJournal/Navbar";
+
+function TJournal() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default TJournal;
